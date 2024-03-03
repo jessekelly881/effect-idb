bb19bac55b03ba77a4d6d2d8b9a3f4709fe220be
Jesse Kelly <kellyjesse881@gmail.com>
2024-03-03 16:49:14 -0600

    chore: added tests

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,32 +1,57 @@
+import * as Error from "@/Error";
 import * as IndexedDB from "@/index";
-import { Effect } from "effect";
+import { Effect, Scope } from "effect";
 import "fake-indexeddb/auto";
-import { describe, it } from "vitest";
+import { TestContext, describe, it } from "vitest";
 
-describe("IndexedDB", () => {
-	it("open", async () => {
-		Effect.runPromise(
-			Effect.gen(function* (_) {
-				const db = yield* _(IndexedDB.IndexedDB);
-				yield* _(
-					db.open({
-						name: "store",
-						version: 1,
-						onUpgrade: (db) =>
-							Effect.gen(function* (_) {
-								yield* _(
-									db
-										.createObjectStore("store")
-										.pipe(Effect.orDie)
-								);
-							})
-					})
-				);
-			}).pipe(
+const test = <A>(
+	name: string,
+	fn: (
+		ctx: TestContext
+	) => Effect.Effect<
+		A,
+		Error.IndexedDBError,
+		Scope.Scope | IndexedDB.IndexedDB
+	>
+) => {
+	it(name, async (ctx) => {
+		const res = await Effect.runPromiseExit(
+			fn(ctx).pipe(
 				Effect.scoped,
 				Effect.provide(IndexedDB.layer),
 				Effect.orDie
 			)
 		);
+		ctx.expect(res._tag).toBe("Success");
 	});
+};
+
+const testDb = Effect.flatMap(IndexedDB.IndexedDB, (db) =>
+	db.open({
+		name: "store",
+		version: 1,
+		onUpgrade: (db) =>
+			Effect.gen(function* (_) {
+				yield* _(db.createObjectStore("store").pipe(Effect.orDie));
+			})
+	})
+);
+
+describe("IndexedDB", () => {
+	test("open", () =>
+		Effect.gen(function* (_) {
+			yield* _(testDb);
+		}));
+
+	test("read/write", (ctx) =>
+		Effect.gen(function* (_) {
+			const db = yield* _(testDb);
+			const res = yield* _(
+				db.transaction(["store"], ({ store }) =>
+					Effect.all([store.add("hello", "key1"), store.get("key1")])
+				)
+			);
+
+			ctx.expect(res[1]).toBe("hello");
+		}));
 });
