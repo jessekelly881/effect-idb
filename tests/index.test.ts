d160735289fdc1668a57028b2f7554488f2eb26c
Jesse Kelly <kellyjesse881@gmail.com>
2024-03-03 16:27:23 -0600

    chore: added tests

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,8 +1,32 @@
-import { fc, test } from "@fast-check/vitest";
-import { describe } from "vitest";
+import * as IndexedDB from "@/index";
+import { Effect } from "effect";
+import "fake-indexeddb/auto";
+import { describe, it } from "vitest";
 
-describe("tests", () => {
-	test.prop([fc.integer(), fc.integer()])("add", (a, b) => {
-		return a + b === b + a;
+describe("IndexedDB", () => {
+	it("open", async () => {
+		Effect.runPromise(
+			Effect.gen(function* (_) {
+				const db = yield* _(IndexedDB.IndexedDB);
+				yield* _(
+					db.open({
+						name: "store",
+						version: 1,
+						onUpgrade: (db) =>
+							Effect.gen(function* (_) {
+								yield* _(
+									db
+										.createObjectStore("store")
+										.pipe(Effect.orDie)
+								);
+							})
+					})
+				);
+			}).pipe(
+				Effect.scoped,
+				Effect.provide(IndexedDB.layer),
+				Effect.orDie
+			)
+		);
 	});
 });
