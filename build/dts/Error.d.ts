074dc6328bd9218de268876ec3d1cdd11188656a
Jesse Kelly <kellyjesse881@gmail.com>
2024-03-03 20:27:24 -0600

    chore: remove build dir from git

--- build/dts/Error.d.ts
+++
@@ -1,9 +0,0 @@
-declare const IndexedDBError_base: new <A extends Record<string, any> = {}>(args: import("effect/Types").Equals<A, {}> extends true ? void : { readonly [P in keyof A as P extends "_tag" ? never : P]: A[P]; }) => import("effect/Cause").YieldableError & {
-    readonly _tag: "IndexedDBError";
-} & Readonly<A>;
-export declare class IndexedDBError extends IndexedDBError_base<{
-    message: string;
-}> {
-}
-export {};
-//# sourceMappingURL=Error.d.ts.map
